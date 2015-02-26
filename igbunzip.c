4d9ad1807b336bfb802ec54f0bd71338313803e6
Robert Blake <rob.c.blake.3@gmail.com>
2015-02-26 10:51:57 -0500

    Adding an unzipping tool

---
+++ igbunzip.c
@@ -0,0 +1,28 @@
+#include <zlib.h>
+#include <stdio.h>
+#include "short_float.h"
+
+int main (int argc, char* argv[])
+{
+  gzFile file;
+  file = gzopen(argv[1], "r");
+  FILE* outfile;
+  outfile = fopen("out.blah.igb", "w");
+
+  char header[1024];
+  gzread(file, header, 1024);
+  fwrite(header, sizeof(char), 1024, outfile);
+  
+  short_float half;
+  while (gzread(file, &half, sizeof(half)) == sizeof(half)) {
+    float ff = floatFromShort(half);
+    fwrite(&ff, sizeof(ff), 1, outfile);
+  }
+  
+
+  gzclose(file);
+  fclose(outfile);
+
+
+  return 0;
+}
